f130e889a8230cab1b5048f245800c35c4d89938
Kip <jeretech.jeremy@gmail.com>
2024-04-15 22:30:39 +0800

    Parsing complete, packaged into a function

--- kivi_parser.py
+++ kivi_parser.py
@@ -15,17 +15,39 @@ FRAME_CHAR = '@'
 # motion data is as follows:
 POSE_CHAR = '>'
 # @X>[A, B, C, D];[A, B, C, D];[A, B, C, D];[0, 0, 0, 1]
+LIST_SEP_CHAR = ';'
+LIST_START_CHAR = '['
+LIST_END_CHAR = ']'
+LIST_ITEM_SEP_CHAR = ", "
 # events such as SYNC flash is indicated by '#'
 EVENT_CHAR = '#'
+
 # so an event would look like this:
 # @X#FLASH
 
+print("hi")
+from mathutils import Matrix, Euler, Vector
+
 
 class Pose:
-    content: str
+    matrix: Matrix
 
-    def __init__(self, _content):
-        self.content = _content
+    def __init__(self, raw_contents: str):
+        # Parse
+
+        # Separate each matrix item
+        items = raw_contents.split(LIST_SEP_CHAR)
+        constructed_matrix_list = []
+        for item in items:
+            # print(f"ITEM {item}")
+            # Reconstruct list
+            list_raw = item[item.index(LIST_START_CHAR) + 1:item.index(LIST_END_CHAR)]
+            list_items = [float(i) for i in list_raw.split(LIST_ITEM_SEP_CHAR)]
+            constructed_matrix_list.append(list_items)
+
+        matrix = Matrix(constructed_matrix_list)
+        # print(f"LIST {constructed_matrix_list} -> MATRIX {matrix}")
+        self.matrix = matrix
 
 
 class Event:
@@ -36,84 +58,82 @@ class Event:
 
 
 # This is a demo parser for a KIVI .mrec motion recording file
-filename = "script1_scene3-exit_shot5.mrec"
+# filename = "script1_scene3-exit_shot5.mrec"
 
-with open(filename) as file:
-    # This reads the entire file into memory. This isn't a big issue for smaller recordings,
-    # plus it's easy to upgrade in the future
-    lines = file.read().splitlines()
 
-    file_checked = False
-    content_started = False
+def parse(filename):
+    with open(filename) as file:
+        # This reads the entire file into memory. This isn't a big issue for smaller recordings,
+        # plus it's easy to upgrade in the future
+        lines = file.read().splitlines()
 
-    file_info = {}
-    file_frames = []
+        file_checked = False
+        content_started = False
 
-    for line in lines:
-        if not file_checked:
-            if line == FIRST_LINE_REQ:
-                file_checked = True
-            else:
-                print(
-                    f"Not a KIVI MOTION RECORDING FILE! {FIRST_LINE_REQ} is expected, but the file starts with {line}")
-                exit()
-        else:
-            if not content_started:
-                # info parse
-                if line[0] == INFO_CHAR:
-                    info_key = line[1:line.index(INFO_SEP_CHAR)]
-                    info_value = line[line.index(INFO_SEP_CHAR) + 1:]
+        file_info = {}
+        file_frames = []
 
-                    # SPECIAL KEYS
-                    if info_key == SHOT_KEY_INT:
-                        info_value = int(info_value)
-                    if info_key == FRAMERATE_KEY_FLOAT:
-                        info_value = float(info_value)
-
-                    file_info[info_key] = info_value
-
-                elif line == START_SYMBOL:
-                    print(file_info)
-                    print(">>>")
-                    content_started = True
-
+        for line in lines:
+            if not file_checked:
+                if line == FIRST_LINE_REQ:
+                    file_checked = True
                 else:
-                    print(f"ERROR! UNEXPECTED SYMBOL IN INFO SECTION: {line}")
-                    exit()
-
+                    raise Exception(
+                        f"Not a KIVI MOTION RECORDING FILE! {FIRST_LINE_REQ} is expected, but the file starts with {line}")
             else:
-                if line[0] == FRAME_CHAR:
-                    if POSE_CHAR in line:
-                        # Pose
+                if not content_started:
+                    # info parse
+                    if line[0] == INFO_CHAR:
+                        info_key = line[1:line.index(INFO_SEP_CHAR)]
+                        info_value = line[line.index(INFO_SEP_CHAR) + 1:]
 
-                        # Get index
-                        frame_index = int(line[1:line.index(POSE_CHAR)])
-                        if frame_index != len(file_frames):
-                            print(f"ERROR! FRAME DATA NOT CONTINUOUS ON {line}")
-                            exit()
+                        # SPECIAL KEYS
+                        if info_key == SHOT_KEY_INT:
+                            info_value = int(info_value)
+                        if info_key == FRAMERATE_KEY_FLOAT:
+                            info_value = float(info_value)
 
-                        # Parse content
-                        pose = Pose(line[line.index(POSE_CHAR) + 1:])
-                        print(pose)
-                        file_frames.append(pose)
+                        file_info[info_key] = info_value
 
-                    elif EVENT_CHAR in line:
-                        # Event
-
-                        # Get index
-                        frame_index = int(line[1:line.index(EVENT_CHAR)])
-                        if frame_index != len(file_frames):
-                            print(f"ERROR! FRAME DATA NOT CONTINUOUS ON {line}")
-                            exit()
-
-                        # Parse event
-                        event = Event(line[line.index(EVENT_CHAR) + 1:])
-                        print(event)
-                        file_frames.append(event)
+                    elif line == START_SYMBOL:
+                        print(file_info)
+                        print(">>>")
+                        content_started = True
 
                     else:
-                        print(f"ERROR! UNEXPECTED CONTENT SYMBOL IN FRAME {line}")
-                else:
-                    print(f"ERROR! UNEXPECTED SYMBOL IN CONTENT SECTION {line}")
+                        raise Exception(f"ERROR! UNEXPECTED SYMBOL IN INFO SECTION: {line}")
 
-file_frames
+                else:
+                    if line[0] == FRAME_CHAR:
+                        if POSE_CHAR in line:
+                            # Pose
+
+                            # Get index
+                            frame_index = int(line[1:line.index(POSE_CHAR)])
+                            if frame_index != len(file_frames):
+                                raise Exception(f"ERROR! FRAME DATA NOT CONTINUOUS ON {line}")
+
+                            # Parse content
+                            pose = Pose(line[line.index(POSE_CHAR) + 1:])
+                            # print(pose)
+                            file_frames.append(pose)
+
+                        elif EVENT_CHAR in line:
+                            # Event
+
+                            # Get index
+                            frame_index = int(line[1:line.index(EVENT_CHAR)])
+                            if frame_index != len(file_frames):
+                                raise Exception(f"ERROR! FRAME DATA NOT CONTINUOUS ON {line}")
+
+                            # Parse event
+                            event = Event(line[line.index(EVENT_CHAR) + 1:])
+                            # print(event)
+                            file_frames.append(event)
+
+                        else:
+                            raise Exception(f"ERROR! UNEXPECTED CONTENT SYMBOL IN FRAME {line}")
+                    else:
+                        raise Exception(f"ERROR! UNEXPECTED SYMBOL IN CONTENT SECTION {line}")
+
+    return file_info, file_frames
